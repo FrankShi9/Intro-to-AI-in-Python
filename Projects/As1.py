dc0169ccfb69ae37ac505f6aeb9bdf3795775366
FrankShi9 <53264047+FrankShi9@users.noreply.github.com>
2021-04-01 20:44:00 +0800

    standard comments

--- Projects/As1.py
+++ Projects/As1.py
@@ -9,15 +9,13 @@ binary_data = np.zeros([100,6],dtype=int)  # the matrix used to store the binary
 N = 100  # This is the same N as specified in the task sheet
 
 
-'''
-loadFiletoMat() function loads the data from .csv file to the numpy matrix
-
-para: input: matrix where the data will be stored
-return void
-'''
-
-
 def load_file_mat(input):
+    """
+    loadFiletoMat() function loads the data from .csv file to the numpy matrix
+
+    :param: input: matrix where the data will be stored
+    :return void
+    """
     cnt = 1
     col,row = 0,0
     while (True):
@@ -37,15 +35,13 @@ def load_file_mat(input):
         cnt += 1
 
 
-'''
-cnt_l() function counts the given l value: 0/1 within the binary data
-
-para: l: value of l to be counted, input: matrix where the data will be read
-return cnt_of_l: total count number of l with value l in the binary data
-'''
-
-
 def cnt_l(l: int, input):
+    """
+    cnt_l() function counts the given l value: 0/1 within the binary data
+
+    :param: l: value of l to be counted, input: matrix where the data will be read
+    :return cnt_of_l: total count number of l with value l in the binary data
+    """
     cnt_of_l = 0
     for index in range(0, 100):
         if input[index][5] == l:
@@ -54,35 +50,31 @@ def cnt_l(l: int, input):
     return cnt_of_l
 
 
-'''
-common_dis() function calculates the common distribution of l 
-with value: 0/1 within the binary data
-
-para: l: value of l to be calculated
-return: common distribution of l = k
-'''
-
-
 def common_dis(l: int):
+    """
+    common_dis() function calculates the common distribution of l
+    with value: 0/1 within the binary data
+
+    :param: l: value of l to be calculated
+    :return: common distribution of l = k
+    """
     if(l==1):
         return (l1 / N) # uses prestored count of l=1 to save time
     else:
         return (l0 / N) # uses prestored count of l=0 to save time
 
 
-'''
-cnt_a() function counts the occurrences of a(i)=a (i,a given as parameters) 
-with l being value: l (l given as parameter) within the binary data
-
-para: i: the subscript of a as the same in the task sheet 
-      a: the value of a(i)
-      l: value of l
-      input: matrix where the data will be read
-return: cnt_of_a: total count number of a(i) with value l=j in the binary data
-'''
-
-
 def cnt_a(i: int, a: int, l: int, input):
+    """
+    cnt_a() function counts the occurrences of a(i)=a (i,a given as parameters)
+    with l being value: l (l given as parameter) within the binary data
+
+    :param: i: the subscript of a as the same in the task sheet
+          a: the value of a(i)
+          l: value of l
+          input: matrix where the data will be read
+    :return: cnt_of_a: total count number of a(i) with value l=j in the binary data
+    """
     cnt_of_a = 0
     for index in range(0, 100):
         if input[index][i] == a and input[index][5] == l:
@@ -90,18 +82,16 @@ def cnt_a(i: int, a: int, l: int, input):
     return cnt_of_a
 
 
-'''
-condi_dis() function calculates the conditional distribution 
-
-para: i: the subscript of a as the same in the task sheet 
-      a: the value of a(i)
-      l: value of l
-      input: matrix where the data will be read
-return: conditional distribution 
-'''
-
-
 def condi_dis(i: int, a: int, l: int, input):
+    """
+    condi_dis() function calculates the conditional distribution
+
+    :param: i: the subscript of a as the same in the task sheet
+          a: the value of a(i)
+          l: value of l
+          input: matrix where the data will be read
+    :return: conditional distribution
+    """
     if(l==0):
         return ((cnt_a(i, a, l, input) / l0))
     else:
